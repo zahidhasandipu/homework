9ef65ebe55d372d4d6d993670310fce9d16b0fb6
zahidhasandipu <zahidhasandipu@gmail.com>
2021-07-10 23:11:05 -0700

    practice

---
+++ app.js
@@ -0,0 +1,35 @@
+// 1. if/else if/else statements #
+
+// var weather = function(temperature){
+//     console.log("The temperature outside is", temperature, "degrees farenheight.");
+    
+//     if(temperature <= 65) {
+//       console.log("It's getting cold outside. Better wear a jacket!");
+//     } else if (temperature > 65 && temperature <= 80){
+//       console.log("It's pleasant outside!");  
+//     } else {
+//       console.log("It's getting hot outside!");
+//     }
+//   }
+  
+//   weather(79);
+
+
+// var weather = function(temperature){
+//     console.log("The temperature outside is", temperature, "degrees farenheight.");
+    
+//     if(temperature <= 30) {
+//       console.log("It's freezing outside! It'll be best to bundle up.");
+//     } else if(temperature <= 55) {
+//       console.log("It's getting cold outside. Better wear a jacket!");
+//     } else if (temperature <= 75){
+//       console.log("It's pleasant outside!");  
+//     } else {
+//       console.log("It's getting hot outside!");
+//     }
+//   }
+  
+//   weather(20);
+//   weather(40);
+//   weather(60);
+//   weather(90);
