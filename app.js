f318a35305330eebfbd7b259425945a8fa400097
zahidhasandipu <zahidhasandipu@gmail.com>
2021-07-11 23:28:08 -0700

    Update app.js

--- app.js
+++ app.js
@@ -85,3 +85,21 @@
 
 // // destructuring
 // const { users, counter } = this.state;
+
+// JavaScript ES5 function
+// function getGreetingFunc() {
+//     return 'Welcome to JavaScript';
+//   }
+  
+//   // JavaScript ES6 arrow function with body
+//   const getGreetingArrow1 = () => {
+//     return 'Welcome to JavaScript';
+//   }
+  
+//   // JavaScript ES6 arrow function without body and implicit return
+//   const getGreetingArrow2 = () =>
+//     'Welcome to JavaScript';
+  
+//   console.log(getGreetingFunc());
+//   console.log(getGreetingArrow1());
+//   console.log(getGreetingArrow2());
